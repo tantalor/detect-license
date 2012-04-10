ff27df3a25ddfaa34663ec171849294764dd77b9
John Tantalo <john.tantalo@gmail.com>
2012-04-09 21:06:24 -0700

    added "test_gpl_type"

--- python/detect_license_test.py
+++ python/detect_license_test.py
@@ -9,7 +9,11 @@ class DetectLicenseTestCase(unittest.TestCase):
   def test_mit_type(self):
     license = detect_license(reader = ['The MIT License', 'Blah blah blah.'])
     self.assertEquals(license['type'], 'MIT License')
-    
+  
+  def test_gpl_type(self):
+    license = detect_license(reader = ['GNU GENERAL PUBLIC LICENSE', 'Blah blah blah.'])
+    self.assertEquals(license['type'], 'GPL')
+  
   def test_year_and_author(self):
     license = detect_license(reader = ['Copyright 2012 Joe Programmer'])
     self.assertEquals(license['author'], 'Joe Programmer')
