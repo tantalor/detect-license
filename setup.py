3783f8db881aca92947f1467535ca60013be0e7c
John Tantalo <john.tantalo@gmail.com>
2012-04-16 19:48:56 -0700

    added package and setup

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+
+from distutils.core import setup
+
+setup(
+  name='detect_license',
+  description='Detects the type, author, and year of a software license.',
+  author='John Tantalo',
+  author_email='john.tantalo@gmail.com',
+  url='http://github.com/tantalor/detect_license',
+  packages=['detect_license'],
+)
